c6f4cb93a0865b15b7f198e76bd89adac60d2cec
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-03-01 17:24:39 +0100

    Added font configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode : "class",
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+        primaryColor : "#111827",
+        primaryColorLight : "rgb(249 250 251)",
+      }
+    }, 
+    container : {
+      center : true,
+      padding :{
+        DEFAULT : '1rem',
+      }
+    },
+    fontFamily :{
+      oswald : ['Oswald', 'sans-serif'],
+      dmsans : ['DM Sans', 'sans-serif'],
+      
+    }
   },
   plugins: [],
-};
+}
