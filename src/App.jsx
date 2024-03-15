07b170e5f835d9ce77c49598a23eba6193a3f369
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-03-15 19:33:48 +0100

    set up new frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import "./App.css";
-import "./responsive.css";
-import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom";
-import {HomeScreen} from "./screens/HomeScreen";
-import {SingleProduct} from "./screens/SingleProduct";
-import {Login} from "./screens/Login";
-import {Register} from "./screens/Register";
-import {CartScreen} from "./screens/CartScreen";
-import {ShippingScreen} from "./screens/ShippingScreen";
-import {ProfileScreen} from "./screens/ProfileScreen";
-import {PaymentScreen} from "./screens/PaymentScreen";
-import {PlaceOrderScreen} from "./screens/PlaceOrderScreen";
-import {OrderScreen} from "./screens/OrderScreen";
+
 /* import NotFound from "./screens/NotFound"; */
+import { Home } from './Pages/Home';
+import {Navbar} from './Components/Navbar';
+import {Footer} from './Components/Footer';
+import { Login } from './Pages/Login';
 
 const App = () => {
   return (
-    <Routes>
-        <Route path="/" element=<HomeScreen/> exact />
-        <Route path="/products/:id" element=<SingleProduct/> />
-        <Route path="/login" element=<Login/> />
-        <Route path="/register" element=<Register/> />
-        <Route path="/profile" element=<ProfileScreen/> />
-        <Route path="/cart/:id?" element=<CartScreen/> />
-        <Route path="/shipping" element=<ShippingScreen/> />
-        <Route path="/payment" element=<PaymentScreen/> />
-        <Route path="/placeorder" element=<PlaceOrderScreen/> />
-        <Route path="/order" element=<OrderScreen/> />
-        {/* <Route path="*" element=<NotFound} /> */}
-    </Routes>
+    <>
+      <Routes>
+        <Route exact path="/home" element={
+          <>
+            <Navbar/>
+            <Home />
+            <Footer />
+          </>
+        }
+        />
+
+        <Route exact path="/login" element={<Login  />} />
+      </Routes>
+    </>
   );
-};
+}
 
 export default App;
+
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,43 +1,7 @@
-import React from "react";
+import React from 'react'
 
 export function Home() {
   return (
-    <div className="px-4 bg-gray-50">
-      <div className="min-h-screen flex items-center">
-        <div className="lg:flex lg:items-center bg-cover bg-center py-16 w-full">
-          <div className="container mx-auto text-black">
-            <div className="lg:flex items-center lg:order-last">
-              <div className="lg:w-1/2 text-center lg:text-left lg:pr-16">
-                <h1 className="text-4xl lg:text-5xl max-w-[700px] font-medium mb-6 lg:text-left">
-                  Welcome to <br />
-                  <span className="text-[#f5deb3] font-bold text-5xl lg:text-7xl">
-                    Solange Luxury Hair Store
-                  </span>
-                </h1>
-                <p className="text-lg lg:text-xl mb-12">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                  viverra euismod odio, gravida pellentesque urna varius vitae.
-                </p>
-                <div className="text-center lg:text-left hover:transition transition-transform hover:scale-105">
-                  <a
-                    href="#"
-                    className="bg-[#f5deb3] text-white py-3 px-6 lg:px-12 font-semibold rounded-lg hover:bg-[#b35478]"
-                  >
-                    Purchase an item
-                  </a>
-                </div>
-              </div>
-              <div className="lg:w-1/2 mt-8 lg:mt-0">
-                <img
-                  src="close-up-portrait-beautiful-teenager.jpg"
-                  className="w-full object-cover rounded-xl h-[300px] lg:h-[420px]"
-                  alt="Layout Image"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
