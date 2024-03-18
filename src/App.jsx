3e4c0fa5aef2194b88e034c04948a91ff66053dc
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-03-18 05:38:13 +0100

    setup homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 /* import NotFound from "./screens/NotFound"; */
 import { Home } from './Pages/Home';
-import {Navbar} from './Components/Navbar';
-import {Footer} from './Components/Footer';
+import { Navbar } from './Components/Navbar';
+import { Footer } from './Components/Footer';
 import { Login } from './Pages/Login';
 
 const App = () => {
+  const BASE_URL =  "https://solange.onrender.com";
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    setError(null); // Clear any previous errors
+
+    try {
+      const response = await fetch(`${BASE_URL}/api/products`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch products');
+      }
+      const data = await response.json();
+      setProducts(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products)
+
   return (
     <>
-      <Routes>
-        <Route exact path="/home" element={
-          <>
-            <Navbar/>
-            <Home />
-            <Footer />
-          </>
-        }
-        />
+      {/* {error && <p>Error: {error}</p>} */}
 
-        <Route exact path="/login" element={<Login  />} />
+      <Routes>
+        <Route exact path="/home" element={<Home products={products} isLoading={isLoading}/>} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Footer } from '../Components/Footer'
+import { Navbar } from '../Components/Navbar'
+import { Products } from '../Components/Products'
 
-export function Home() {
+export function Home({ products }) {
   return (
-    <div>Home</div>
+    <section>
+      <Navbar />
+
+      <div className="w-screen h-screen text-white bg-primaryColor"/* style={{
+        background: "linear-gradient(90deg, rgba(131, 126, 226, 1) 24%, rgba(114, 114, 226, 1) 58%, rgba(0, 212, 255, 1) 100%)"
+      }} */>
+        <div className="container mx-auto flex items-center justify-evenly flex-row-reverse">
+          <div>
+            <img className="lg:w-[520px] md:w-[350px] w-[250px]" alt="hero" src="/images/_hero.png" />
+          </div>
+
+          <div className="lg:w-5/12 w-full">
+            <h1 className="my-4 text-5xl font-bold leading-tight">
+              Turn your designs into production-ready frontend
+            </h1>
+            <p className="text-2xl mb-8">
+              Ship products 5-10x faster with your existing design tools, tech stacks & workflows!
+            </p>
+            <div className="flex mx-auto">
+              <button
+                className="hover:underline bg-white text-gray-800 font-bold rounded-full  py-4 px-8">
+                View Projects
+              </button>
+              <button
+                className="ml-4 hover:underline bg-white text-gray-800 font-bold rounded-full  py-4 px-8">
+                Plugins
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </div >
+
+      <Products products={products} />
+      <Footer />
+    </section>
   )
 }
