b0a9255c7ef634b25a2088a1451b0e24692ba981
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-03-25 12:55:48 +0100

    Revert "updated cart page"
    This reverts commit 2c5f471c8209ec0e2eb72ec924cd1464c9433354.

--- src/Redux/Actions/CartActions.js
+++ src/Redux/Actions/CartActions.js
@@ -5,7 +5,9 @@ const BASE_URL = "https://solange.onrender.com";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const response = await fetch(`${BASE_URL}/api/products/${id}`);
-
+    if (!response.ok) {
+        throw new Error('Failed to get product');
+    }
     const data = await response.json();
 
     dispatch({
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,26 +1,19 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { productDetailReducer, productListReducer } from './Reducers/ProductReducers'
 import { cartReducer } from './Reducers/CartReducers'
-import { userLoginReducer } from './Reducers/UserReducers';
 
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetail: productDetailReducer,
-    cart: cartReducer,
-    userLogin: userLoginReducer
+    productList : productListReducer,
+    productDetail : productDetailReducer,
+    cart : cartReducer,
 })
 
-const cartItemsFromLocalStorage = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []
-
-const userInfoFromLocalStorage = localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfoo")) : null
+const cartItemsFromLocalStorage = localStorage.getItem("cartItem") ? JSON.parse(localStorage.getItem("cartItem")) : []
 
 
 const initialState = {
-    cart: {
-        cartItems: cartItemsFromLocalStorage
-    },
-    userLogin: {
-        userInfo: userInfoFromLocalStorage
+    cart : {
+        cartItems : cartItemsFromLocalStorage
     }
 }
 
