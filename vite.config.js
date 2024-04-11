2c343d28a19253d2017e0f473b479560b98b69fe
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-04-11 06:54:14 +0100

    updated proxy

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,26 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      "/api/": "https://solange-backend.onrender.com/",
+      "/uploads/": "https://solange-backend.onrender.com/",
+    },
+  },
+});
+
+
+
+/* import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
       "/api/": "http://localhost:5000",
       "/uploads/": "http://localhost:5000",
     },
   },
 });
+ */
+
